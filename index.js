5217d1aded9023cf2cad718def13bf57109b66b4
chrischep <christine.chepleting@student.moringaschool.com>
2022-08-29 21:26:30 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    const userName = {
+        name: name,
+        email: email
+    };
+   return fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json",
+                'accept': "application/json"
+            },
+
+            body: JSON.stringify(userName),
+        })
+        .then(response => response.json())
+        .then(user => {
+            const userid = document.createElement('h1');
+            const body = document.querySelector('body');
+            userid.textContent = user.id;
+            body.appendChild(userid);
+
+        })
+        .catch(error => {
+        alert("error occurred"); 
+        });
+}
